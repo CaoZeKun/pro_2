e5e6ce2f75b8bf51f98e3547762cfdd6b1d639d1
the <984297068@qq.com>
2019-01-10 21:39:08 +0800

    copy - por_1 0.0.0

--- model/pytorch/tourial_chatbot.py
+++ model/pytorch/tourial_chatbot.py
@@ -368,6 +368,71 @@ max_target_len: 10
 """
 
 
+class EncoderRNN(nn.Module):
+    def __init__(self, hidden_size, embedding, n_layers=1, dropou=0):
+        super(EncoderRNN,self).__init__()
+        self.n_layers = n_layers
+        self.hidden_size =hidden_size
+        self.embedding = embedding
+        # Initialize GRU; the input_size and hidden_size params are both set to 'hidden_size'
+        #   because our input size is a word embedding with number of features == hidden_size
+        self.gru = nn.GRU(hidden_size, hidden_size,n_layers,
+                          dropout=(0 if n_layers==1 else dropou), bidirectional=True)
+
+    def forward(self, input_seq, input_lengths, hidden=None):
+        # Convert word indexes to embeddings
+        embedded = self.embedding(input_seq)
+        packed = torch.nn.utils.rnn.pack_padded_sequence(embedded, input_seq)
+        outputs, hidden = self.gru(packed, hidden)
+        outputs, _ = torch.nn.utils.rnn.pad_packed_sequence(outputs)
+        # sum bidirectional Gru outputs
+        # outputs: output features from the last hidden layer of the GRU (sum of bidirectional outputs);
+        # shape=(max_length, batch_size, hidden_size)
+        outputs = outputs[:,:,:self.hidden_size] + outputs[:,:,self.hidden_size:]
+        return outputs,hidden
+
+    class Attn(torch.nn.Module):
+        def __init__(self, method, hidden_size):
+            super(Attn, self).__init__()
+            self.method = method
+            if self.method not in ['dot', 'general', 'concat']:
+                raise ValueError(self.method, "is not an appropriate attention method.")
+            self.hidden_size = hidden_size
+            if self.method == 'general':
+                self.attn = torch.nn.Linear(self.hidden_size, hidden_size)
+            elif self.method == 'concat':
+                self.attn = torch.nn.Linear(self.hidden_size*2, hidden_size)
+                self.v = torch.nn.Parameter(torch.FloatTensor(hidden_size))
+
+        def dot_socre(self, hidden, encoder_output):
+            return torch.sum(hidden * encoder_output, dim=2)
+
+
+        def general_score(self,hidden,encoder_output):
+            energy = self.attn(encoder_output)
+            return torch.sum(hidden * energy, dim =2)
+
+
+        def concat_score(self,hidden,encoder_output):
+            energy = self.attn(torch.cat((hidden.expand(encoder_output.size(0),-1,-1),encoder_output),
+                                         2)).tanh()
+
+
+        def forowrd(self, hidden, encoder_outputs):
+            if self.method == 'general':
+                attn_energies = self.general_score(hidden,encoder_outputs)
+            elif self.method == 'concat':
+                attn_energies = self.concat_score(hidden,encoder_outputs)
+            elif self.method == 'dot':
+                attn_energies = self.dot_socre(hidden,encoder_outputs)
+
+            # Transpose max_length and batch_size dimensions
+            attn_energies = attn_energies.t()
+
+            # return the softmax normalized probability scores (with added dimension)
+            return F.softmax(attn_energies, dim=1).unsqueeze(1)
+
+
 
 
 
